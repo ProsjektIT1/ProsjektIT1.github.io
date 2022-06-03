455c412a2fae4adee0786217b85ec1d2212758eb
TeodorLB <lolgamez92@gmail.com>
2022-06-04 01:46:14 +0200

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,16 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish(
-	'__sapper__/export',
+	"__sapper__/export",
 	{
-		branch: 'master',
+		branch: "master",
 		repo: "https://github.com/prosjektit1/prosjektit1.github.io.git",
 		user: {
-			name: 'ProsjektIT1',
-			email: 'lolgamez92@gmail.com'
-		}
+			name: "ProsjektIT1",
+			email: "lolgamez92@gmail.com",
+		},
 	},
 	() => {
-		console.log('Deploy Complete!')
+		console.log('Deploy Complete!');
 	}
-)
+);

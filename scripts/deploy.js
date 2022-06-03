681872aa6aa644dbda04f4cde203e1a60f3b57cd
TeodorLB <lolgamez92@gmail.com>
2022-06-04 00:22:54 +0200

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const ghpages = require('gh-pages');
 ghpages.publish(
 	'__sapper__/export',
 	{
-		branch: 'master',
+		branch: 'main',
 		repo: "prosjektit1.github.io",
 		user: {
 			name: 'ProsjektIT1',

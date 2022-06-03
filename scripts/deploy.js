a43237d2d164b11cfe0d0c49f0b98d2c1fdfe035
TeodorLB <lolgamez92@gmail.com>
2022-06-04 01:35:36 +0200

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,13 +3,8 @@ const ghpages = require('gh-pages');
 ghpages.publish(
 	'__sapper__/export',
 	{
-<<<<<<< Updated upstream
 		branch: 'balls',
-		repo: "https://github.com/ProsjektIT1/ProsjektIT1.github.io.git",
-=======
-		branch: 'master',
 		repo: "https://github.com/prosjektit1/prosjektit1.github.io.git",
->>>>>>> Stashed changes
 		user: {
 			name: 'ProsjektIT1',
 			email: 'lolgamez92@gmail.com'

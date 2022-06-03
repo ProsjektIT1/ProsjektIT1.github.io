a775389f47540283e10de44e99f3a0deaad83238
TeodorLB <lolgamez92@gmail.com>
2022-06-03 23:37:12 +0200

    stuff
    please work

---
+++ ProsjektIT1/scripts/deploy.js
@@ -0,0 +1,16 @@
+const ghpages = require('gh-pages');
+
+ghpages.publish(
+	'__sapper__/export',
+	{
+		branch: 'master',
+		repo: ProsjektIT1.github.io,
+		user: {
+			name: 'ProsjektIT1',
+			email: 'lolgamez92@gmail.com'
+		}
+	},
+	() => {
+		console-log('Deploy Complete!')
+	}
+)
